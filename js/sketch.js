56c1e118a4b7fa57dffad0753ad663ddea9be3a5
Mohammad Mehdi <rsmohammadmehdi@gmail.com>
2018-10-29 00:27:53 +0330

    feat: add search function

--- js/sketch.js
+++ js/sketch.js
@@ -1,53 +1,63 @@
 let tree
 let container = []
 
-function setup(){
+function setup() {
     tree = new Tree()
-    for(let i = 0;i < 100;i++){
-        tree.addValue(floor(random(0,100)))
+    for (let i = 0; i < 100; i++) {
+        tree.addValue(floor(random(0, 100)))
     }
     tree.traverse()
+    tree.search(floor(random(0,100)))
     console.log(container)
 }
 
 // ----------Tree----------
 // Create
-function Tree(){
+function Tree() {
     this.root = null
 }
 
 // Add value 
-Tree.prototype.addValue = function(value) {
+Tree.prototype.addValue = function (value) {
     let node = new Node(value)
-    if(this.root ===  null){
+    if (this.root === null) {
         this.root = node
     } else {
         this.root.addNode(node)
     }
 }
 // Traverse
-Tree.prototype.traverse = function() {
+Tree.prototype.traverse = function () {
     this.root.visit()
+}
+// Search
+Tree.prototype.search = function (value) {
+    let found = this.root.search(value)
+    if (found != null) {
+        console.log('Found ' + value)
+    } else if (found == null) {
+        console.log('Not Found')
+    }
 }
 
 // ----------Node----------
 // Create
-function Node(value){
+function Node(value) {
     this.value = value
     this.left = null
     this.right = null
 }
 
 // Add Node
-Node.prototype.addNode = function(node) {
-    if(node.value < this.value) {
-        if(this.left === null) {
+Node.prototype.addNode = function (node) {
+    if (node.value < this.value) {
+        if (this.left === null) {
             this.left = node
         } else {
             this.left.addNode(node)
         }
-    } else if(node.value > this.value) {
-        if(this.right === null) {
+    } else if (node.value > this.value) {
+        if (this.right === null) {
             this.right = node
         } else {
             this.right.addNode(node)
@@ -55,12 +65,23 @@ Node.prototype.addNode = function(node) {
     }
 }
 // Visit
-Node.prototype.visit = function(){
-    if(this.left !== null){
+Node.prototype.visit = function () {
+    if (this.left !== null) {
         this.left.visit()
     }
     container.push(this.value)
-    if(this.right !== null){
+    if (this.right !== null) {
         this.right.visit()
     }
+}
+// Search 
+Node.prototype.search = function (value) {
+    if (value == this.value) {
+        return value
+    } else if (value < this.value && this.left != null) {
+        return this.left.search(value)
+    } else if (value > this.value && this.right != null) {
+        return this.right.search(value)
+    }
+    return null
 }
